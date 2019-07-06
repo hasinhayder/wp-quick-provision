bf4a227e31efeede2f6e1c373bc4078b97ab5c32
Hasin Hayder <countdraculla@gmail.com>
2019-07-06 22:03:36 +0600

    option section label and invalid gist url check

--- wp-quick-provision.php
+++ wp-quick-provision.php
@@ -18,6 +18,7 @@ add_action( 'plugins_loaded', function () {
 add_action( 'admin_menu', function () {
 	add_menu_page( 'WP Quick Provision', 'Quick Provision', 'manage_options', 'wpqp', function () {
 
+		$wpqp_proceed = true;
 		include_once( ABSPATH . 'wp-admin/includes/theme.php' );
 		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		include_once( ABSPATH . 'wp-admin/includes/file.php' );
@@ -26,16 +27,32 @@ add_action( 'admin_menu', function () {
 
 		?>
         <h2><?php _e( 'WordPress Quick Provision', 'wp-quick-provision' ); ?></h2>
+
+
         <form method="POST">
 
-            <label for="gist"><strong><?php _e( 'Your GIST Configuration URL', 'wp-quick-provision' ); ?></strong></label><br/>
-            <input type="text" style="font-size: 15px;margin-top:20px;" name="gist" id="gist" cols="70"
-                   rows="5" placeholder="<?php _e('configuration gist url','wp-quick-provision'); ?>"/><br/>
+            <label for="gist"><strong><?php _e( 'Gist URL', 'wp-quick-provision' ); ?></strong></label><br/>
+            <input type="text" style="width:60%;font-size: 15px;margin-top:10px; line-height: 30px;" name="gist"
+                   id="gist" cols="70"
+                   rows="5" placeholder="<?php _e( 'Gist URL with Provision Data', 'wp-quick-provision' ); ?>"/><br/>
+			<?php
+			if ( isset( $_POST['submit'] ) ) {
+				$wpqp_gist_url = strtolower( sanitize_text_field( $_POST['gist'] ) );
+				if ( strpos( $wpqp_gist_url, "gist" ) === false ) {
+					$wpqp_proceed = false;
+					?>
+                    <div class="notice notice-error is-dismissible" style="margin-left: 1px;">
+                        <p><?php _e( "Invalid gist URL", 'wp-quick-provision' ); ?></p>
+                    </div>
+					<?php
+				}
+			}
+			?>
 			<?php echo submit_button( __( 'Start Provisioning', 'wp-quick-provision' ) ); ?>
         </form>
 		<?php
 
-		if ( isset( $_POST['submit'] ) ) {
+		if ( isset( $_POST['submit'] ) && $wpqp_proceed ) {
 			$wpqp_theme_installer  = new Theme_Upgrader();
 			$wpqp_plugin_installer = new Plugin_Upgrader();
 
@@ -54,7 +71,7 @@ add_action( 'admin_menu', function () {
 				$wpqp__theme = strtolower( trim( $wpqp_theme ) );
 				if ( ! array_key_exists( $wpqp__theme, $wpqp_installed_themes ) ) {
 					?>
-                    <div class="notice notice-success">
+                    <div class="notice notice-success" style="margin-left: 1px;">
                         <p><?php printf( __( "<strong>Installing Theme %s</strong>", 'wp-quick-provision' ), $wpqp__theme ); ?></p>
                         <p>
 							<?php
@@ -66,7 +83,7 @@ add_action( 'admin_menu', function () {
 
 				} else {
 					?>
-                    <div class="notice notice-error">
+                    <div class="notice notice-error" style="margin-left: 1px;">
                         <p><?php printf( __( "Theme <strong>%s</strong> is already installed", 'wp-quick-provision' ), $wpqp__theme ); ?></p>
                     </div>
 					<?php
@@ -80,7 +97,7 @@ add_action( 'admin_menu', function () {
 				$wpqp__plugin = strtolower( trim( $wpqp_plugin ) );
 				if ( ! array_key_exists( $wpqp__plugin, $wpqp_installed_plugins ) ) {
 					?>
-                    <div class="notice notice-success">
+                    <div class="notice notice-success" style="margin-left: 1px;">
                         <p><?php printf( __( "<strong>Installing Plugin %s</strong>", 'wp-quick-provision' ), $wpqp__plugin ); ?></p>
                         <p>
 							<?php
@@ -92,7 +109,7 @@ add_action( 'admin_menu', function () {
 
 				} else {
 					?>
-                    <div class="notice notice-error">
+                    <div class="notice notice-error" style="margin-left: 1px;">
                         <p><?php printf( __( 'Plugin <strong>%s</strong> is already installed', 'wp-quick-provision' ), $wpqp__plugin ); ?></p>
                     </div>
 					<?php
@@ -101,13 +118,13 @@ add_action( 'admin_menu', function () {
 
 			do_action( "wpqp_plugins_installed" );
 
-			$wpqp_installed_plugins = process_keys( array_keys( get_plugins() ) );
+			$wpqp_installed_plugins = wpqp_process_keys( array_keys( get_plugins() ) );
 			echo '<h2>' . __( 'Activating Plugins', 'wp-quick-provision' ) . '</h2>';
 			foreach ( $wpqp_plugins as $wpqp_plugin ) {
 				$wpqp__plugin = strtolower( trim( $wpqp_plugin ) );
 				activate_plugin( $wpqp_installed_plugins[ $wpqp__plugin ] );
 				?>
-                <div class="notice notice-success">
+                <div class="notice notice-success" style="margin-left: 1px;">
                     <p><?php printf( __( "</strong>%s</strong> is Activated <br/>", 'wp-quick-provision' ), $wpqp__plugin ); ?></p>
                 </div>
 				<?php
@@ -115,6 +132,9 @@ add_action( 'admin_menu', function () {
 
 			do_action( "wpqp_plugins_activated" );
 
+			if(count($wpqp_options)>0){
+				echo '<h2>' . __( 'Updating Options', 'wp-quick-provision' ) . '</h2>';
+            }
 			foreach ( $wpqp_options as $_okey => $_ovalue ) {
 				update_option( $_okey, $_ovalue );
 			}
